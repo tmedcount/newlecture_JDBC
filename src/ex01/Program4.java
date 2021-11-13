b9f3697c570635700503bd8588590263e1d69042
yamto <yamto@121.152.88.198>
2021-11-13 18:06:55 +0900

    데이터 삭제

---
+++ src/ex01/Program4.java
@@ -0,0 +1,35 @@
+package ex01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+public class Program4 {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		int id = 13;
+		
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		String sql = "DELETE NOTICE WHERE ID = ?";
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection(url, "newlec", "rufdml13");
+		// Statement st = con.createStatement();
+		// st.ex...(sql)
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, id);
+		
+		int result = st.executeUpdate();
+		
+		System.out.print(result);
+		
+		st.close();
+		con.close();		
+	}
+
+}
