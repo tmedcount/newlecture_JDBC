543cf31b85b1cfbf63741d81f28398112f1a1ec2
yamto <yamto@211.229.210.166>
2021-11-25 08:38:57 +0900

    페이징 쿼리 이용하기

--- src/com/newlecture/app/service/NoticeService.java
+++ src/com/newlecture/app/service/NoticeService.java
@@ -18,14 +18,24 @@ public class NoticeService {
 	private String pwd = "rufdml13";
 	private String driver = "oracle.jdbc.driver.OracleDriver";
 	
-	public List<Notice> getList() throws ClassNotFoundException, SQLException {
+	public List<Notice> getList(int page) throws ClassNotFoundException, SQLException {
 		
-		String sql = "SELECT * FROM NOTICE";
+		int start =  1 + (page-1)*10;// 1, 11, 21... 
+		int end = 10 * page; // 10, 20, 30...		
+		
+		String sql = "SELECT * FROM ("
+				+ "    SELECT ROWNUM NUM, N.* FROM ("
+				+ "        SELECT * FROM NOTICE ORDER BY REGDATE DESC"
+				+ "            ) N"
+				+ ")"
+				+ " WHERE NUM BETWEEN ? AND ?";
 		
 		Class.forName(driver);
 		Connection con = DriverManager.getConnection(url, uid, pwd);
-		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery(sql);
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, start);
+		st.setInt(2, end);
+		ResultSet rs = st.executeQuery();
 		
 		List<Notice> list = new ArrayList<Notice>();
 		
