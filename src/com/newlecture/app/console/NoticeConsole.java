04d714e846da39ce267024c846378a9511711937
yamto <yamto@211.229.210.166>
2021-11-24 21:00:23 +0900

    공지사항 메뉴 붙이기

--- src/com/newlecture/app/console/NoticeConsole.java
+++ src/com/newlecture/app/console/NoticeConsole.java
@@ -2,6 +2,7 @@ package com.newlecture.app.console;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 import com.newlecture.app.entity.Notice;
 import com.newlecture.app.service.NoticeService;
@@ -34,8 +35,14 @@ public class NoticeConsole {
 	}
 
 	public int inputNoticeMenu() {
-		// TODO Auto-generated method stub
-		return 0;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.printf("1.상세조회/ 2.이전/ 3.다음/ 4.글쓰기/ 5.종료 >");
+		
+		String menu_ = scan.nextLine();
+		int menu = Integer.parseInt(menu_);
+		
+		return menu;
 	}
 
 }
--- src/ex01/Program5.java
+++ src/ex01/Program5.java
@@ -9,21 +9,28 @@ public class Program5 {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		NoticeConsole console = new NoticeConsole();
 		
-		console.printNoticeList();
-		
-		int menu = console.inputNoticeMenu();
-		
-		switch(menu) {
-		case 1: // 상세조회
-			break;
-		case 2: // 이전
-			break;
-		case 3: // 다음
-			break;
-		case 4: // 글쓰기
-			break;
+		EXIT:
+		while(true) {
+			console.printNoticeList();
+			int menu = console.inputNoticeMenu();
+			
+			switch(menu) {
+			case 1: // 상세조회
+				break;
+			case 2: // 이전
+				break;
+			case 3: // 다음
+				break;
+			case 4: // 글쓰기
+				break;
+			case 5: // 종료
+				System.out.println("Bye~~");
+				break EXIT;
+			default:
+				System.out.println("<<사용방법>> 메뉴는 1~4까지만 입력할 수 있습니다.");
+				break;
+			}
 		}
-
 	}
 
 }
